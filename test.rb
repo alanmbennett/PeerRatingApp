e094537dfb08c11ca632136582356c7b5ea2e0de
Alan B <alanmitchellbennett@gmail.com>
2018-11-28 10:42:23 -0500

    Add voting.rb
    Currently has functions for voting and checks to see if votes already exist, also updated peerratingdb.db to include Websites and VotedFor tables to store website and vote information respectively

--- test.rb
+++ test.rb
@@ -8,3 +8,17 @@ db.execute("select * from Users") do |result|
   print result
 end
 
+#db.execute("CREATE TABLE Websites (filePath varchar(255), orderOfUpload int);");
+#db.execute("CREATE TABLE VotedFor (username varchar(50), filePath varchar(255), rank int);");
+
+puts "---"
+
+db.execute("select * from Websites") do |result|
+  print result
+end
+
+puts "---"
+
+db.execute("select * from VotedFor") do |result|
+  print result
+end
